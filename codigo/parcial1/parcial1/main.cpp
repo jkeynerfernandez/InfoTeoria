99a82d4b6f134733f289250f5c744a8cf751cf67
jkeynerfernandez <fernandezdj54@gmail.com>
2023-04-20 14:49:05 -0500

    etsba creando la matriz pero recordé que en la practica dos había hecho algo parceido

--- codigo/parcial1/parcial1/main.cpp
+++ codigo/parcial1/parcial1/main.cpp
@@ -2,23 +2,64 @@
 #include <iostream>
 using namespace std;
 
+void diasSemana();
+void semana();
 
 int main()
 {
-    char* data;
-    ifstream archivoConHorario;
-    archivoConHorario.open("C:/Users/keyner/Desktop/horario.txt");
+    cout << "hola mundo ";
 
-    archivoConHorario>>data;
-    for(int i = 1; i <= 10000; i++)
-     {
-     archivoConHorario>>data;
-     cout << data << endl;
-     }
-    cout<<data;
-    archivoConHorario.close();
+       semana();
+//    char* data;
+//    ifstream archivoConHorario;
+//    archivoConHorario.open("C:/Users/keyner/Desktop/horario.txt");
+
+//    archivoConHorario>>data;
+//    for(int i = 1; i <= 10000; i++)
+//     {
+//     archivoConHorario>>data;
+//     cout << data << endl;
+//     }
+//    cout<<data;
+//    archivoConHorario.close();
 
     return 0;
 }
+//void semana ( ){
+//    for(int dia =1 ;dia  <=7; dia++){
+
+//        cout << diasSemana(dia)<<endl;
+//        for (float hora=0.00; hora<=23.00;hora ++){
+//            cout << hora <<endl;
+
+//        }
 
 
+
+//    }
+//}
+
+//void diasSemana(int dia){
+//    switch (dia) {
+//    case 1: cout<< "Lunes";
+//        break;
+//    case 2: cout<< "MARTES";
+//        break;
+//    case 3: cout<< "MIERCOLES";
+//        break;
+//    case 4: cout<< "JUEVES";
+//        break;
+//    case 5: cout<< "VIERNES";
+//        break;
+//    case 6: cout<< "SABADO";
+//        break;
+//    case 7: cout<< "DOMINGO";
+//        break;
+
+//    default:
+//        break;
+
+
+//    }
+
+//}
